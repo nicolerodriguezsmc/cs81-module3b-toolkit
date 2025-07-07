36b45ee01bd8f56a5c17e9bb4c9cbc817206aa01
Nicole Rodriguez <rodriguez_nicole_l01@student.smc.edu>
2025-07-06 21:33:28 -0700

    Add math.js with square, isEven, isOdd, and multiply functions

--- math.js
+++ math.js
@@ -2,3 +2,23 @@
 function double(num) {
     return num * 2;
 }
+
+//function that returns the square of a number
+function square(num) {
+    return num * num;
+}
+
+//function that tests if a number is even
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+//function that tests if a number is odd
+function isOdd(num) {
+    return num % 2 !== 0;
+}
+
+//function that multiplies two numbers together.
+function multiply(num1, num2) {
+    return num1 * num2;
+}
